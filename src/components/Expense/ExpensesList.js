ac69befca7c8708adb22e44b4516810ee692b791
anjalikhandelwal18 <anjalikhandelwal1999@gmail.com>
2023-01-06 12:25:50 +0530

    Initial commit expenses-dev

---
+++ src/components/Expense/ExpensesList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ExpenseItems from './ExpenseItems';
+import './ExpensesList.css';
+
+function ExpensesList(props)
+{
+
+    if(props.expenses.length === 0)
+    {
+        return (<h2 className="expenses-list__fallback">No Expenses Found</h2>)
+    }
+    
+    return(
+        <ul className="expenses-list">
+          {props.expenses.map((expense) => 
+          <ExpenseItems 
+             key={expense.id}
+             title={expense.title} 
+             amount={expense.amount} 
+             date={expense.date}
+          />
+          )}
+        </ul>
+    )
+}
+
+export default ExpensesList;
